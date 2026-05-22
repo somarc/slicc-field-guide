0b691189ba5801b843db2b68d89839d7d87c4ed1
Marc Hess <8710267+somarc@users.noreply.github.com>
2026-05-22 08:43:35 -0400

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,19 @@
 module.exports = {
-  root: true,
-  extends: 'airbnb-base',
-  env: {
-    browser: true,
-  },
-  parser: '@babel/eslint-parser',
-  parserOptions: {
-    allowImportExportEverywhere: true,
-    sourceType: 'module',
-    requireConfigFile: false,
-  },
-  rules: {
-    'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
-    'linebreak-style': 0, // disabled: GitHub web editor always commits CRLF
-    'no-param-reassign': [2, { props: false }], // allow modifying properties of param
-  },
+root: true,
+extends: 'airbnb-base',
+env: {
+browser: true,
+},
+parser: '@babel/eslint-parser',
+parserOptions: {
+allowImportExportEverywhere: true,
+sourceType: 'module',
+requireConfigFile: false,
+},
+rules: {
+'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
+'linebreak-style': 0, // disabled: GitHub web editor always commits CRLF
+'max-len': 0, // disabled: boilerplate and custom code have long lines
+'no-param-reassign': [2, { props: false }], // allow modifying properties of param
+},
 };
